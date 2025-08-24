c020425f38e82420601249a992278f379a4195a1
SawyDust1228 <yinshuo991229@163.com>
2025-08-24 03:25:39 -0400

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Shuo Yin, Su Zheng, Ziyang Yu, Bei Yu",
     author_email="syin22@cse.cuhk.edu.hk",
 
-    url="https://github.com/SawyDust1228/OpenLitho.git",
+    url="https://github.com/OpenOPC/TorchLitho-Lite",
 
     packages=find_packages(),
 
